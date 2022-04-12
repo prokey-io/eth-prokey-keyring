503f6cd8e9ae95da6279fea18a02aa9bc4f84d94
Houman Sanati <houmansnt@gmail.com>
2022-04-12 12:45:49 +0430

    forget device,remove account,sign transaction implemented

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 /* eslint-disable no-unused-vars */
 const { EventEmitter } = require('events');
 const ethUtil = require('ethereumjs-util');
+const { TransactionFactory } = require('@ethereumjs/tx');
 const HDKey = require('hdkey');
 
 const PROKEY_LINK_URL = 'http://localhost:4200';
 
-const hdPathString = `m/44'/1'/0'/0`;
+const hdPathString = `m/44'/60'/0'/0`;
 const keyringType = 'Prokey Hardware';
 const pathBase = 'm';
 const MAX_INDEX = 1000;
+const DELAY_BETWEEN_POPUPS = 2000;
 
 const {
   concatSig,
@@ -28,13 +30,18 @@ const CommandType = {
   GetAddresses: 'GetAddresses',
 };
 
+// eslint-disable-next-line jsdoc/require-jsdoc
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 class ProkeyKeyring extends EventEmitter {
   constructor(opts = {}) {
     super();
     this.type = keyringType;
     this._wallets = [];
     this.accounts = [];
-    this.hdk = new HDKey();
+    this.hdKey = new HDKey();
     this.page = 0;
     this.perPage = 5;
     this.unlockedAccount = 0;
@@ -84,6 +91,24 @@ class ProkeyKeyring extends EventEmitter {
       .publicToAddress(dkey.publicKey, true)
       .toString('hex');
     return ethUtil.toChecksumAddress(`0x${address}`);
+  }
+
+  _pathFromAddress(address) {
+    const checksummedAddress = ethUtil.toChecksumAddress(address);
+    let index = this.paths[checksummedAddress];
+    if (typeof index === 'undefined') {
+      for (let i = 0; i < MAX_INDEX; i++) {
+        if (checksummedAddress === this._addressFromIndex(pathBase, i)) {
+          index = i;
+          break;
+        }
+      }
+    }
+
+    if (typeof index === 'undefined') {
+      throw new Error('Unknown address');
+    }
+    return `${this.hdPath}/${index}`;
   }
 
   __getPage(increment) {
@@ -190,12 +215,125 @@ class ProkeyKeyring extends EventEmitter {
     return Promise.resolve(this.accounts.slice());
   }
 
+  forgetDevice() {
+    this.accounts = [];
+    this.hdk = new HDKey();
+    this.page = 0;
+    this.unlockedAccount = 0;
+    this.paths = {};
+  }
+
+  removeAccount(address) {
+    if (
+      !this.accounts.map((a) => a.toLowerCase()).includes(address.toLowerCase())
+    ) {
+      throw new Error(`Address ${address} not found in this keyring`);
+    }
+
+    this.accounts = this.accounts.filter(
+      (a) => a.toLowerCase() !== address.toLowerCase(),
+    );
+  }
+
   // tx is an instance of the ethereumjs-transaction class.
-  async signTransaction(address, tx, opts = {}) {
-    const privKey = this._getPrivateKeyFor(address, opts);
-    const signedTx = tx.sign(privKey);
-    // Newer versions of Ethereumjs-tx are immutable and return a new tx object
-    return signedTx === undefined ? tx : signedTx;
+  signTransaction(address, tx) {
+    // transactions built with older versions of ethereumjs-tx have a
+    // getChainId method that newer versions do not. Older versions are mutable
+    // while newer versions default to being immutable. Expected shape and type
+    // of data for v, r and s differ (Buffer (old) vs BN (new))
+    if (typeof tx.getChainId === 'function') {
+      // In this version of ethereumjs-tx we must add the chainId in hex format
+      // to the initial v value. The chainId must be included in the serialized
+      // transaction which is only communicated to ethereumjs-tx in this
+      // value. In newer versions the chainId is communicated via the 'Common'
+      // object.
+      return this._signTransaction(address, tx.getChainId(), tx, (payload) => {
+        tx.v = payload.v;
+        tx.r = payload.r;
+        tx.s = payload.s;
+        return tx;
+      });
+    }
+    // For transactions created by newer versions of @ethereumjs/tx
+    // Note: https://github.com/ethereumjs/ethereumjs-monorepo/issues/1188
+    // It is not strictly necessary to do this additional setting of the v
+    // value. We should be able to get the correct v value in serialization
+    // if the above issue is resolved. Until then this must be set before
+    // calling .serialize(). Note we are creating a temporarily mutable object
+    // forfeiting the benefit of immutability until this happens. We do still
+    // return a Transaction that is frozen if the originally provided
+    // transaction was also frozen.
+    const unfrozenTx = TransactionFactory.fromTxData(tx.toJSON(), {
+      common: tx.common,
+      freeze: false,
+    });
+    unfrozenTx.v = new ethUtil.BN(
+      ethUtil.addHexPrefix(tx.common.chainId()),
+      'hex',
+    );
+    return this._signTransaction(
+      address,
+      tx.common.chainIdBN().toNumber(),
+      unfrozenTx,
+      (payload) => {
+        // Because tx will be immutable, first get a plain javascript object that
+        // represents the transaction. Using txData here as it aligns with the
+        // nomenclature of ethereumjs/tx.
+        const txData = tx.toJSON();
+        // The fromTxData utility expects v,r and s to be hex prefixed
+        txData.v = ethUtil.addHexPrefix(payload.v);
+        txData.r = ethUtil.addHexPrefix(payload.r);
+        txData.s = ethUtil.addHexPrefix(payload.s);
+        // Adopt the 'common' option from the original transaction and set the
+        // returned object to be frozen if the original is frozen.
+        const a = TransactionFactory.fromTxData(txData, {
+          common: tx.common,
+          freeze: Object.isFrozen(tx),
+        });
+        return a;
+      },
+    );
+  }
+
+  // tx is an instance of the ethereumjs-transaction class.
+  async _signTransaction(address, chainId, tx, handleSigning) {
+    try {
+      const deviceStatus = await this.unlock();
+      await wait(deviceStatus === 'just unlocked' ? DELAY_BETWEEN_POPUPS : 0);
+      const txParams = {
+        path: this._pathFromAddress(address),
+        transaction: {
+          to: this._normalize(tx.to),
+          value: this._normalize(tx.value),
+          gasPrice: this._normalize(tx.gasPrice),
+          gasLimit: this._normalize(tx.gasLimit),
+          nonce: this._normalize(tx.nonce),
+          data: this._normalize(tx.data),
+          chainId,
+        },
+      };
+      const response = await this.runCommandOnProkeyLink(
+        txParams,
+        CommandType.SignTransaction,
+      );
+      const newOrMutatedTx = handleSigning(response);
+
+      const addressSignedWith = ethUtil.toChecksumAddress(
+        ethUtil.addHexPrefix(newOrMutatedTx.getSenderAddress().toString('hex')),
+      );
+      const correctAddress = ethUtil.toChecksumAddress(address);
+      if (addressSignedWith !== correctAddress) {
+        throw new Error("signature doesn't match the right address");
+      }
+
+      return newOrMutatedTx;
+    } catch (e) {
+      throw new Error((e && e.toString()) || 'Unknown error');
+    }
+  }
+
+  _normalize(buf) {
+    return ethUtil.bufferToHex(buf).toString();
   }
 
   // For eth_sign, we need to sign arbitrary data:
@@ -215,103 +353,12 @@ class ProkeyKeyring extends EventEmitter {
     return sig;
   }
 
-  // For eth_decryptMessage:
-  async decryptMessage(withAccount, encryptedData) {
-    const wallet = this._getWalletForAccount(withAccount);
-    const privateKey = ethUtil.stripHexPrefix(wallet.privateKey);
-    const sig = decrypt({ privateKey, encryptedData });
-    return sig;
+  signTypedData() {
+    return Promise.reject(new Error('Not supported on this device'));
   }
 
-  // personal_signTypedData, signs data along with the schema
-  async signTypedData(
-    withAccount,
-    typedData,
-    opts = { version: SignTypedDataVersion.V1 },
-  ) {
-    // Treat invalid versions as "V1"
-    const version = Object.keys(SignTypedDataVersion).includes(opts.version)
-      ? opts.version
-      : SignTypedDataVersion.V1;
-
-    const privateKey = this._getPrivateKeyFor(withAccount, opts);
-    return signTypedData({ privateKey, data: typedData, version });
-  }
-
-  // get public key for nacl
-  async getEncryptionPublicKey(withAccount, opts = {}) {
-    const privKey = this._getPrivateKeyFor(withAccount, opts);
-    const publicKey = getEncryptionPublicKey(privKey);
-    return publicKey;
-  }
-
-  _getPrivateKeyFor(address, opts = {}) {
-    if (!address) {
-      throw new Error('Must specify address.');
-    }
-    const wallet = this._getWalletForAccount(address, opts);
-    return wallet.privateKey;
-  }
-
-  // returns an address specific to an app
-  async getAppKeyAddress(address, org) {
-    if (!org || typeof org !== 'string') {
-      throw new Error(`'origin' must be a non-empty string`);
-    }
-    const wallet = this._getWalletForAccount(address, {
-      withAppKeyOrigin: org,
-    });
-    const appKeyAddress = normalize(
-      ethUtil.publicToAddress(wallet.publicKey).toString('hex'),
-    );
-    return appKeyAddress;
-  }
-
-  // exportAccount should return a hex-encoded private key:
-  async exportAccount(address, opts = {}) {
-    const wallet = this._getWalletForAccount(address, opts);
-    return wallet.privateKey.toString('hex');
-  }
-
-  removeAccount(address) {
-    if (
-      !this._wallets
-        .map(({ publicKey }) =>
-          ethUtil.bufferToHex(ethUtil.publicToAddress(publicKey)).toLowerCase(),
-        )
-        .includes(address.toLowerCase())
-    ) {
-      throw new Error(`Address ${address} not found in this keyring`);
-    }
-
-    this._wallets = this._wallets.filter(
-      ({ publicKey }) =>
-        ethUtil
-          .bufferToHex(ethUtil.publicToAddress(publicKey))
-          .toLowerCase() !== address.toLowerCase(),
-    );
-  }
-
-  _getWalletForAccount(account, opts = {}) {
-    const address = normalize(account);
-    let wallet = this._wallets.find(
-      ({ publicKey }) =>
-        ethUtil.bufferToHex(ethUtil.publicToAddress(publicKey)) === address,
-    );
-    if (!wallet) {
-      throw new Error('Simple Keyring - Unable to find matching address.');
-    }
-
-    if (opts.withAppKeyOrigin) {
-      const { privateKey } = wallet;
-      const appKeyOriginBuffer = Buffer.from(opts.withAppKeyOrigin, 'utf8');
-      const appKeyBuffer = Buffer.concat([privateKey, appKeyOriginBuffer]);
-      const appKeyPrivateKey = ethUtil.keccak(appKeyBuffer, 256);
-      const appKeyPublicKey = ethUtil.privateToPublic(appKeyPrivateKey);
-      wallet = { privateKey: appKeyPrivateKey, publicKey: appKeyPublicKey };
-    }
-
-    return wallet;
+  exportAccount() {
+    return Promise.reject(new Error('Not supported on this device'));
   }
 }
 
